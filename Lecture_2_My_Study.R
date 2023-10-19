2fc3d48f5c772fcc3d59ad8f75f934cf1e5950dd
maksoy88bjk <maksoy88bjk@gmail.com>
2023-10-19 11:46:59 +0300

    send

--- Lecture_2_My_Study.R
+++ Lecture_2_My_Study.R
@@ -12,4 +12,49 @@ library(nycflights13)
 
 short_flights<- flights |>
   filter(air_time < 60)
-edsdsds
+
+# Data Check ----------------------------------------------------
+
+head(flights)
+str(flights)
+glimpse(flights)
+
+# Data Transformations ----------------------------------------------------
+
+### destination MIA olan ayni gundeki gecikmelerin ortalamalarini aldik
+flights |> 
+  filter(dest == "MIA") |> 
+  group_by(year,month, day) |> 
+  summarize(
+    gecikme = mean(arr_delay, na.rm = TRUE)
+  )
+
+### 11 Ocak'daki United Airlines Ucuslari 
+jan11 <- flights |> 
+  filter(month == 1 & day == 11 & carrier == "UA")
+
+### %in% means and & or 
+
+flights |> 
+  filter(month %in% c(1, 2))
+
+### 1 veya 2 
+
+flights |> 
+  filter(month == 1 | month == 2)
+
+### arrange
+## The arrange() reorder rows, either ascending (default) or descending
+
+flights |> 
+  arrange(year, month, day, arr_time)
+
+flights |> 
+  arrange(desc(dep_delay))
+
+
+
+
+
+
+
