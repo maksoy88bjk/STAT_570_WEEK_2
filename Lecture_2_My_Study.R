25bbb7af269a857463e900e1d214611aa8fc674f
maksoy88bjk <maksoy88bjk@gmail.com>
2023-10-19 11:07:51 +0300

    add project organization to directory

---
+++ Lecture_2_My_Study.R
@@ -0,0 +1,14 @@
+# Lecture 3 ---------------------------------------------------------------
+
+rm(list=ls())
+# We will be working on Data Transformation for this lecture
+
+# Packages ----------------------------------------------------------------
+
+library(tidyverse)
+library(nycflights13)
+
+# Descriptives  -----------------------------------------------------------
+
+short_flights<- flights |>
+  filter(air_time < 60)
